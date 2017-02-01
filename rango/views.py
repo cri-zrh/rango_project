c9fbd2779261048db6d17c63ed0a166175a96b0d
cri-zrh <cri-zrh>
2017-02-01 10:47:35 +0000

    end of chapter 7

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-# Import category and page model
+from rango.forms import CategoryForm, PageForm
 from rango.models import Category, Page
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -30,6 +30,45 @@ def show_category(request, category_name_slug):
 		context_dict['pages'] = None
 
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			cat = form.save(commit=True)
+			print(cat, cat.slug)
+			#form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+
+	context_dict = {'form': form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
+
 
 #	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about'>About</a>")
 
